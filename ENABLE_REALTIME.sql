6c1cee274897aa771b3e30366f808968eb6b84c1
Izuchukwu Tony <izuchukwuonuoha6@gmail.com>
2025-12-01 07:16:06 +0100

    fix

--- ENABLE_REALTIME.sql
+++
@@ -1,5 +0,0 @@
--- Enable realtime replication for items table
-ALTER PUBLICATION supabase_realtime ADD TABLE items;
-
--- Enable realtime replication for admins table
-ALTER PUBLICATION supabase_realtime ADD TABLE admins;
